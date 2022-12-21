28bef9082626f8291595c63e4c01cf0df82706c4
13464 <daurentk@gmail.com>
2022-12-20 20:42:33 -0600

    Home page created

---
+++ src/webapp/HomePage.java
@@ -0,0 +1,9 @@
+package webapp;
+
+public class HomePage {
+public String purpose = "Practicing day 02 Git lessons";
+public int qty = 1;
+    // Setting up a homepage
+    // navi menu
+    // following Dr. Saim
+}
